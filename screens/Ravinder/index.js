e0910ba767b4f65bb451438c811e25d366907342
ravinder yadav <ravinder.y@crowdbotics.com>
2024-01-18 16:36:14 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Ravinder/index.js
+++ screens/Ravinder/index.js
@@ -1,3 +1,7 @@
+import { CheckBox } from "react-native-elements";
+import { ActivityIndicator } from "react-native";
+import { Switch } from "react-native";
+import { RadioGroup } from "react-native-radio-buttons-group";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
@@ -8,13 +12,37 @@ const Ravinder = () => {
       padding: 10,
       position: "relative",
       flex: 1
-    }}></ScrollView>
+    }}><RadioGroup style={styles.BHNVuols} radioButtons={[{
+        id: "1",
+        label: "Option 1",
+        value: "option1"
+      }, {
+        id: "2",
+        label: "Option 2",
+        value: "option2"
+      }]} layout="column"></RadioGroup><Switch style={styles.ARZyXxXw} value={true}></Switch><ActivityIndicator style={styles.BUIayRWm}></ActivityIndicator><CheckBox style={styles.ORDciBwB} title="Checkbox Title" checked={true}></CheckBox></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: "100%"
+  },
+  BHNVuols: {
+    width: 120,
+    height: 70
+  },
+  ARZyXxXw: {
+    width: 50,
+    height: 25
+  },
+  BUIayRWm: {
+    width: 50,
+    height: 50
+  },
+  ORDciBwB: {
+    width: 183,
+    height: 69
   }
 });
 export default Ravinder;
